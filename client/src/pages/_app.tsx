69fd04f6b919ddc7eee9f90fa0b8838654e78e9b
Anton Kolot <antvelkol@gmail.com>
2023-07-28 18:28:28 +0300

    Added work with themes

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,31 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { NextUIProvider } from '@nextui-org/react';
+import { createTheme, NextUIProvider } from '@nextui-org/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import Header from '@/components/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const lightTheme = createTheme({
+    type: 'light',
+    theme: {},
+  });
+
+  const darkTheme = createTheme({
+    type: 'dark',
+    theme: {},
+  });
+
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>);
+    <NextThemesProvider
+      defaultTheme='system'
+      attribute='class'
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className,
+      }}>
+      <NextUIProvider>
+        <Header />
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </NextThemesProvider>);
 }
