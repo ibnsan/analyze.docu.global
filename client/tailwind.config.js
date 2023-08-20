6d52e0bf46ea5ddd9b367731d0c00375241a9431
ibnsan <antvelkol@gmail.com>
2023-08-20 19:03:06 +0200

    Update tailwind.config.js
    Added variables for global colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,7 +7,22 @@ module.exports = {
     './src/**/*.{ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        aquahaze: '#EBF2F5',
+        solitude: '#D0E1E7',
+        matisse: '#0070A5',
+        orient: '#0A5479',
+        bluewhale: '#06314B',
+        shark: '#222222',
+        emperor: '#4F4D4D',
+        gray: '#8A8686',
+        cloud: '#CCCACA',
+        mercury: '#E5E3E3',
+        alabaster: '#F5F5F5',
+        tomato: '#FF6337'
+      },
+    },
   },
   plugins: [],
 }
